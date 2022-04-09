2324987ae85106b21373b6e43bde86344603e152
Ade Rizky <ade@MacBook-Pro.local>
2022-04-09 17:10:35 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# uas_191011401763
+
+Nama : Ade Rizki Awaludin
+
+NIM : 191011401763
+
+Kelas : 06TPLE019
+
+UAS Mobile Proggramming
+
+## Soal Tugas
+
+### 1. Apa yang dimaksud dengan state management pada flutter?
+
+State Management adalah sebuah cara untuk mengatur data dalam flutter agar dapat lebih terstruktur dan rapi.
+Cara kerja State Management dalam contoh Provider yaitu kita bisa memasukan state yang mungkin dapat berubah, lalu Widget yang sudah kita buat akan berubah sesuai dengan data yang ada.
+
+### 2. Sebutkan apa saja state management pada flutter !
+
+Pakage State Management pada flutter saat ini:
+
+1. Provider, Adalah state management yang menyediakan sebuah teknik mengolah state yang dapat digunakan untuk memanage data didalam aplikasi.
+
+2. Riverpod, Adalah state management yang sama dengan Provider hanya saja kita tidak akan menemukan ProviderNotFoundException atau error dalam menangani proses dalam mengelola state.
+
+3. Redux, yaitu berasal dari javascript yang membuat predictable state container untuk aplikasi.
+
+4. Flutter Commands
+
+5. Binder
+
+6. GetX
+
+7. states_rebuilder
+
+8. Triple Pattern (Segmented State Pattern)
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+# uas_191011401763
+
+# uas_191011401763
---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MaterialApp(
+    onGenerateRoute: route.generateRoute,
+  ));
+}
+
+class route {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    // jika ingin mengirim argument
+    // final args = settings.arguments;
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => HomePage());
+      case '/listloop':
+        return MaterialPageRoute(builder: (_) => jsonList());
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(title: Text("ERROR")),
+        body: Center(child: Text('Page Error')),
+      );
+    });
+  }
+}
+
+class HomePage extends StatelessWidget {
+  String button1 = "HALAMAN TAMPILAN JSON to UI";
+
+  @override
+  Widget build(BuildContext context) {
+    void button1press() {
+      Navigator.pushNamed(context, '/listloop');
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PAGE HOME'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                onPressed: button1press,
+                child: Text(button1),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class jsonList extends StatelessWidget {
+  final String apiUrl = "http://jsonplaceholder.typicode.com/posts";
+  Future<List<dynamic>> _fecthDataUsers() async {
+    var result = await http.get(apiUrl);
+    return json.decode(result.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ADE RIZKI AWALUDIN | 191011401763'),
+      ),
+      body: Container(
+        child: FutureBuilder<List<dynamic>>(
+          future: _fecthDataUsers(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  padding: EdgeInsets.all(10),
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        radius: 30,
+                        child: Text((snapshot.data[index]['id'].toString())),
+                      ),
+                      title: Text(snapshot.data[index]['title'] +
+                          " (" +
+                          snapshot.data[index]['userId'].toString() +
+                          ")"),
+                      subtitle: Text(snapshot.data[index]['body']),
+                    );
+                  });
+            } else {
+              return Scaffold(
+                body: Center(child: CircularProgressIndicator()),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
